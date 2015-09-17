731241807c3e660ee74f8fe932617e08357e04ee
Jacob Talley <jacob.talley@hotmail.com>
2015-09-17 16:03:31 -0600

    adding blog posts to fb

--- js/admin/adminCtrl.js
+++ js/admin/adminCtrl.js
@@ -1,3 +1,30 @@
-angular.module('myApp').controller('adminCtrl', function(){
+angular.module('myApp').controller('adminCtrl', function($scope, adminService, $firebaseArray){
+	// var ref = new Firebase('https://chrisblashillphoto.firebaseio.com/');
+
+	var url = 'https://chrisblashillphoto.firebaseio.com/';
+
+	$scope.items = $firebaseArray(new Firebase(url + '/items'));
+	$scope.allData = false;
+	$scope.allDataTrue= false;
+
+	$scope.addPost = function(postDate, postPost, postTitle){
+		if (postDate && postTitle && postPost){
+	 		$scope.items.$add({date:postDate, post:postPost,title:postTitle});
+	 		$scope.allData = false;
+	 		$scope.allDataTrue = true;
+	 		$scope.date = "";
+	 		$scope.post = "";
+	 		$scope.title= "";
+
+	 	}
+
+	 	else if (!postDate || !postPost || !postTitle){
+	 		$scope.allData = true;
+	 	}
+
+
+
 	
+	 }
+
 })
--- js/admin/adminService.js
+++ js/admin/adminService.js
@@ -1,11 +1,8 @@
 angular.module('myApp').service('adminService', function(){
-	 this.addquote= function(mydate, mytitle, mypost){
-  			if (date && title && post){
-  				blogService.blogPost.push({
-  					date: mydate,
-  					title: mytitle,
-  					post: mypost
-  				})
-  			}
-  		}
+
+
+
+
+
+
 })
