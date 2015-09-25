ffecfdad4605e229ecc37449a1d6ffa5cdf2de87
Jacob Talley <jacob.talley@hotmail.com>
2015-09-25 10:26:27 -0600

    almost fully mobile compatible

--- js/directives.js
+++ js/directives.js
@@ -17,3 +17,21 @@ angular.module('myApp')
 	}
 })
 
+.directive('hoverBlue', function(){
+	return{
+		restrict:'A',
+		scope: {
+			toggle:'@',
+			toggleOff:'@'
+		},
+		link: function (scope, elem, attrs){
+			elem.on('mouseover', function(){
+				elem.addClass(scope.toggle)
+			});
+			elem.on('mouseout', function(){
+				elem.removeClass(scope.toggle)
+			})
+		}
+	}
+})
+
